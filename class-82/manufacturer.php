ad926b59f38fbb4524313a9e0536529d39a3af4a
asiahub-10 <asiarahman104@gmail.com>
2026-05-23 13:02:26 +0600

    craat

---
+++ class-82/manufacturer.php
@@ -0,0 +1,104 @@
+<?php
+require_once "db-config.php";
+
+// Create Data
+if(isset($_POST['add_mfg'])) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $active = isset($_POST['active']) ? 1 : 0;
+
+    $db->query("insert into manufactures(name, address, is_active) values('$name', '$address', $active)");
+}
+
+// Delete Data
+if(isset($_POST['delete_id'])) {
+    $id = $_POST['delete_id'];
+    $db->query("delete from manufactures where id = $id");
+}
+
+// Read Data
+$result = $db->query("select * from manufactures");
+if ($result) {
+    $mfg = $result->fetch_all(MYSQLI_ASSOC);
+    // echo "<pre>";
+    // print_r($mfg);
+    // echo "</pre>";
+} else {
+    echo $db->error;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manufacturer</title>
+</head>
+
+<body>
+    <nav>
+        <a href="manufacturer.php">Manufacturer</a> |
+        <a href="product.php">Product</a>
+    </nav>
+
+    <div style="display: flex; gap: 100px;">
+        <div>
+            <h3>Add New Manufacturer</h3>
+            <form method="POST">
+                <label for="name">Name</label><br>
+                <input type="text" name="name">
+                <br><br>
+                <label for="address">Address</label><br>
+                <textarea name="address"></textarea>
+                <br><br>
+                <input type="checkbox" name="active" id="active">
+                <label for="active">Is active</label>
+                <br><br>
+                <button type="submit" name="add_mfg">Save</button>
+            </form>
+        </div>
+        <div>
+            <h3>Manufacturers List</h3>
+            <table border="1" width="100%" cellspacing="0" cellpadding="5">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (isset($mfg)):
+                        foreach ($mfg as $item):
+                            $status = $item['is_active'] ? "Active" : "Inactive";
+                    ?>
+                            <tr>
+                                <td><?= $item['id']; ?></td>
+                                <td><?= $item['name']; ?></td>
+                                <td><?= $item['address']; ?></td>
+                                <td><?= $status; ?></td>
+                                <td>
+                                    <form method="POST">
+                                        <input type="hidden" name="delete_id" value="<?= $item['id']; ?>">
+                                        <button type="submit">Delete</button>  
+                                    </form>                                    
+                                </td>
+                            </tr>
+                    <?php
+                        endforeach;
+                    endif;
+                    ?>
+                </tbody>
+
+            </table>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ class-82/round_70.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2026 at 09:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `round_70`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `manufactures`
+--
+
+CREATE TABLE `manufactures` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(4) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `manufactures`
+--
+
+INSERT INTO `manufactures` (`id`, `name`, `address`, `is_active`) VALUES
+(4, 'Dell', 'US', 0),
+(5, 'HP', 'n/a', 1),
+(6, 'Samsung', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `manufacturer_id` int(11) NOT NULL,
+  `product_code` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `manufactures`
+--
+ALTER TABLE `manufactures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_code` (`product_code`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `manufactures`
+--
+ALTER TABLE `manufactures`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
